76d9a0f98472262166bc8f1ff9b7f1173f1b280e
csh <tmdgjs7495@naver.com>
2023-05-02 11:52:58 +0900

    [feat] : WebIgnore 설정

--- core/src/main/java/com/core/springsecurity/io/core/security/security/configs/SecurityConfig.java
+++ core/src/main/java/com/core/springsecurity/io/core/security/security/configs/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.core.springsecurity.io.core.security.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +19,50 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsService userDetailsService() {
+
+        String password = passwordEncoder().encode("1111");
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(password)
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(password)
+                .roles("ADMIN")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("manager")
+                .password(password)
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin, sys);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .authorizeRequests(authorize -> authorize
+                        .mvcMatchers("/").permitAll()
+                        .mvcMatchers("/mypage").hasRole("USER")
+                        .mvcMatchers("/messages").hasRole("MANAGER")
+                        .mvcMatchers("/config").hasRole("ADMIN")
                         .anyRequest().authenticated());
 
         http
